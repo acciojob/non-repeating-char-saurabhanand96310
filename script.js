24fa0c33b94b420dca274c3ad82ccbcad53e6220
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-01-12 18:18:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,5 +16,5 @@ function firstNonRepeatedChar(str) {
 		return "null"
 	}
 }
-//const input = prompt("Enter a string");
+const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
